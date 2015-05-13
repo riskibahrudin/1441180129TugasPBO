8d981a76b1abed85df0615a441867740a3ff6bc8
riskibahrudin <riskibahrudin09@gmail.com>
2015-05-13 16:59:44 +0700

    UTS
    tugas om

---
+++ UTS/Robot.java
@@ -0,0 +1,24 @@
+class Robot
+{
+	int sensor = 4;
+	int servo;
+	int power;
+	int camera;
+
+	public int getSensor()
+	{
+		return sensor;
+	}
+	public int getServo()
+	{
+		return servo;
+	}
+	public int getPower()
+	{
+		return power;
+	}
+	public int getCamera()
+	{
+		return camera;
+	}
+}
